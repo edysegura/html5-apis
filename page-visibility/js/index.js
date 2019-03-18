8d31593cb28ac17149d95fff9a95c69017eb1784
Edy Segura <edysegura@gmail.com>
2019-03-18 15:49:34 -0300

    Refactor page visibility example

--- page-visibility/js/index.js
+++ page-visibility/js/index.js
@@ -1,10 +1,12 @@
 'use strict'
 
-document.addEventListener('visibilitychange', () => {
+const logCurrentState = () => {
   const documentStates = {
     prerender: 'Page is prerendered!',
     hidden: 'Page is hidden!',
     visible: 'Page is visible!'
   }
   console.log(documentStates[ document.visibilityState ])
-})
+}
+
+document.addEventListener('visibilitychange', logCurrentState)
