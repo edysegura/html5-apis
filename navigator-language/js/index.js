e4a8270c0dae612a0deeec9e2718a28426c21aab
Edy Segura <edysegura@gmail.com>
2019-04-08 22:20:12 -0300

    Add index.js implementation for language example

--- navigator-language/js/index.js
+++ navigator-language/js/index.js
@@ -1,2 +1,10 @@
-console.log(navigator.language)
-console.log(navigator.languages)
+'use strict'
+
+function addLanguage(language) {
+  const ol = document.querySelector('ol')
+  const li = document.createElement('li')
+  li.textContent = language
+  ol.appendChild(li)
+}
+
+navigator.languages.forEach(addLanguage)
