93e61be493c36f202dc22f2c9a548011b80a0da8
Edy Segura <edysegura@gmail.com>
2019-04-11 23:02:31 -0300

    Add fetch implementation

--- fetch/js/index.js
+++ fetch/js/index.js
@@ -1,8 +1,19 @@
 'use strict'
 
-;(async function loadData() {
+function addToList(pokemon) {
+  const ol = document.querySelector('ol')
+  const li = document.createElement('li')
+  li.textContent = pokemon.name
+  ol.appendChild(li)
+}
+
+function showPokemons(pokemons) {
+  pokemons.forEach(addToList)
+}
+
+;(async function fetchData() {
   const endpoint = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=151'
   const response = await fetch(endpoint)
   const data = await response.json()
-  console.log(data)
+  showPokemons(data.results)
 })()
