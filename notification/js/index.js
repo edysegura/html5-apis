befd967fe36a22e6e4df39d6e02d7371b431f5e4
Edy Segura <edysegura@gmail.com>
2019-04-17 23:04:49 -0300

    Add index.js implementation for Notification example

---
+++ notification/js/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const [ enableButton, notificationButton ] = document.querySelectorAll('button')
+
+function enableNotification() {
+  Notification.requestPermission()
+    .then(permission => console.log(permission))
+}
+
+function showNotification() {
+  const notificationParams = {
+    body: 'This is the notification body, enjoy using this feature!',
+    icon: 'https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_48-512.png'
+  }
+  const notification = new Notification('Warning', notificationParams)
+  const delay = 5000
+  setTimeout(() => notification.close(), delay)
+}
+
+enableButton.addEventListener('click', () => enableNotification())
+notificationButton.addEventListener('click', () => showNotification())
