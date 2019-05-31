63bf879602fdd861dfffc968231f73133a611e29
Edy Segura <edysegura@gmail.com>
2019-05-30 22:58:12 -0300

    Refactor word count component

--- custom-elements/components/word-count.component.js
+++ custom-elements/components/word-count.component.js
@@ -2,10 +2,10 @@ class WordCount extends HTMLElement {
   constructor() {
     super()
     this.root = this.attachShadow({ mode: 'open' })
-    this.setText(this.getAttribute('text'))
+    this.render(this.getAttribute('text'))
   }
 
-  setText(text) {
+  render(text) {
     this.root.innerHTML = `
       ${ this.style() }
       <p>${ text } <span>has ${ this.count(text) } word(s)</span></p>
