76292e471303d64114740febe1690585ef423ed8
Ednardo David Segura <ednardo@inatel.br>
2019-06-25 22:24:38 -0300

    Add index.html for Intl API example

--- internationalization/index.js
+++ internationalization/index.js
@@ -1,9 +1,9 @@
 const date = new Date('2018-03-11T21:10:00')
+const [ enUsSpan, enGbSpan ] = document.querySelectorAll('span')
 
 function formatDate(date, locale) {
   return new Intl.DateTimeFormat(locale).format(date)
 }
 
-console.log(formatDate(date, 'en-US'))
-console.log(formatDate(date, 'en-GB'))
-console.log(formatDate(date, 'pt-BR'))
+enUsSpan.textContent = formatDate(date, 'en-US')
+enGbSpan.textContent = formatDate(date, 'en-GB')
