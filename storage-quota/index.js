9077f34d81e5a49ed16ddbf36b81eeb9f07e516a
Edy Segura <edysegura@gmail.com>
2019-09-25 16:52:23 -0300

    Create a class to organize the code

--- storage-quota/index.js
+++ storage-quota/index.js
@@ -1,11 +1,26 @@
 'use strict'
 
-if ('storage' in navigator && 'estimate' in navigator.storage) {
-  navigator.storage.estimate()
-    .then(estimate => {
-      document.getElementById('usage').textContent = (estimate.usage / 1024) / 1024;
-      document.getElementById('quota').textContent = (estimate.quota / 1024) / 1024;
-      document.getElementById('percent').textContent =
-        (estimate.usage * 100 / estimate.quota).toFixed(0);
-    });
+class App {
+  constructor() {
+    this.estimateStorageQuota()
+  }
+
+  async estimateStorageQuota() {
+    const estimate = await navigator.storage.estimate()
+    this.showValue('usage', estimate.usage)
+    this.showValue('quota', estimate.quota)
+    this.calculateUsage(estimate)
+  }
+
+  showValue(elementId, value) {
+    document.getElementById(elementId).textContent =
+      ((value / 1024) / 1024).toFixed(2)
+  }
+
+  calculateUsage({ usage, quota }) {
+    document.getElementById('percent').textContent =
+      (usage * 100 / quota).toFixed(0)
+  }
 }
+
+new App()
