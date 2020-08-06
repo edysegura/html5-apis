d5b9e35ec760d37574b37bed40b7d23349942890
Edy Segura <edysegura@gmail.com>
2020-08-06 20:23:18 -0300

    Refactor xml-http-request example

--- xml-http-request/index.js
+++ xml-http-request/index.js
@@ -1,6 +1,11 @@
 'use strict'
 
+function showResponse(json) {
+  const pre = document.querySelector('pre')
+  pre.textContent = json
+}
+
 const request = new XMLHttpRequest()
 request.open('GET', 'https://api.github.com/users/edysegura')
-request.addEventListener('load', event => console.log(event.target.responseText))
+request.addEventListener('load', event => showResponse(event.target.responseText))
 request.send()
