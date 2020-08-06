fdac5355c3f92a752da248833b0f062b204f28d4
Edy Segura <edysegura@gmail.com>
2020-08-06 20:20:45 -0300

    Refactor index.js
    - Use github api

--- xml-http-request/index.js
+++ xml-http-request/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
 const request = new XMLHttpRequest()
-request.open('GET', 'https://reqres.in/api/users')
+request.open('GET', 'https://api.github.com/users/edysegura')
 request.addEventListener('load', event => console.log(event.target.responseText))
 request.send()
