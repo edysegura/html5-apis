4a2e8a00ac37413f4e13b21c720324df86245c93
Edy Segura <edysegura@gmail.com>
2020-09-21 22:26:34 -0300

    Add local storage example

---
+++ local-storage/app.js
@@ -0,0 +1,41 @@
+class App {
+
+  constructor() {
+    this.setupButton();
+    this.listLocalStorageValues();
+  }
+
+  setupButton() {
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+      this.saveToStorage();
+    });
+  }
+
+  saveToStorage() {
+    const keyInput = document.getElementById('key');
+    const valueInput = document.getElementById('value');
+    if (keyInput.value && valueInput.value) {
+      localStorage.setItem(keyInput.value, valueInput.value);
+      this.listLocalStorageValues();
+      keyInput.value = valueInput.value = "";
+    }
+  }
+
+  listLocalStorageValues() {
+    const lsValues = document.getElementById('lsValues');
+
+    const toHtml = (key) => {
+      const value = localStorage.getItem(key);
+      return `<p>${key}: ${value}</p>`;
+    };
+
+    const htmlOutput = Object.keys(localStorage)
+      .map(toHtml)
+      .join('');
+
+    lsValues.innerHTML = htmlOutput;
+  }
+}
+
+new App();
