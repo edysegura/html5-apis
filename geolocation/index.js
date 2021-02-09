7ce82f91d95800b2f13da04a06c2206951cd17a8
Edy Segura <edysegura@gmail.com>
2021-02-08 21:10:53 -0300

    Move first fetch example under pokemon folder

--- geolocation/index.js
+++ geolocation/index.js
@@ -44,8 +44,3 @@ for (const button of buttons) {
   })
 }
 
-async function fetchCep() {
-  const response = await fetch('https://viacep.com.br/ws/37540000/json/unicode/')
-  const data = await response.json()
-  console.log(data)
-}
