1fb9dc1ae121540a01c3b00f1d2cb7b18509f2d0
Edy Segura <edysegura@gmail.com>
2022-04-04 22:28:31 -0300

    feat: implement cep example

--- fetch/cep/index.js
+++ fetch/cep/index.js
@@ -1,7 +1,18 @@
-async function fetchCep() {
-  const response = await fetch('https://viacep.com.br/ws/37540000/json/unicode/')
-  const data = await response.json()
-  console.log(data)
+const form = document.querySelector('form')
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  showCepData('loading...')
+  showCepData(await fetchCep(form.cep.value))
+})
+
+function showCepData(cepData) {
+  const pre = document.querySelector('pre')
+  pre.textContent = JSON.stringify(cepData, null, 2)
 }
 
-fetchCep()
+async function fetchCep(cep) {
+  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  const data = await response.json()
+  return data
+}
