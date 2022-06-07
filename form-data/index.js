ceaa8b59f11128ae51a32e3bcf1e452acb7ca790
Edy Segura <edysegura@gmail.com>
2022-06-06 22:07:13 -0300

    chore: update FormData API example

--- form-data/index.js
+++ form-data/index.js
@@ -1,9 +1,9 @@
-const form = document.querySelector('form');
+const form = document.querySelector('form')
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const data = new FormData(form);
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const data = new FormData(form)
   for (const key of data.values()) {
-    console.log(key);
+    console.log(key)
   }
-});
+})
