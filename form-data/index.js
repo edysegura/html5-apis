6c1b3bafe77ad3068a510558751ff272cea57f5b
Edy Segura <edysegura@gmail.com>
2022-06-15 22:09:08 -0300

    feat: update form-data example to show values in HTML

--- form-data/index.js
+++ form-data/index.js
@@ -1,10 +1,14 @@
 const form = document.querySelector('form')
+const pre = document.querySelector('pre')
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   const data = new FormData(form)
   const keys = new Set(data.keys())
+  const formValues = []
   for (const key of keys) {
     console.log(data.getAll(key))
+    formValues.push({ field: key, values: data.getAll(key) })
   }
+  pre.textContent = JSON.stringify(formValues, null, 2)
 })
