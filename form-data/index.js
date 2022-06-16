a1fe7f773ac45b42b7d26040a5fa2a86eb7cef0b
Edy Segura <edysegura@gmail.com>
2022-06-15 21:59:20 -0300

    refactor: form-data example

--- form-data/index.js
+++ form-data/index.js
@@ -3,7 +3,8 @@ const form = document.querySelector('form')
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   const data = new FormData(form)
-  for (const key of data.values()) {
-    console.log(key)
+  const keys = new Set(data.keys())
+  for (const key of keys) {
+    console.log(data.getAll(key))
   }
 })
