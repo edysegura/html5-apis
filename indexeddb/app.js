3e401956f5d9bc29826241c7adea5abcc44c2f8f
Edy Segura <edysegura@gmail.com>
2023-04-10 23:25:33 -0300

    feat: update indexeddb example to use poke api

--- indexeddb/app.js
+++ indexeddb/app.js
@@ -1,23 +1,25 @@
 import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@3.2.3/+esm'
 
-let db = new Dexie('todoDB')
+let db = new Dexie('pokemonDB')
 
 db.version(1).stores({
-  tasks: '++id,userId,title,completed',
+  pokemon: '++id,name',
 })
 
 db.on('populate', async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-  const todoList = await response.json()
-  await db.tasks.bulkPut(todoList)
+  const response = await fetch(
+    'https://pokeapi.co/api/v2/pokemon?offset=0&limit=151',
+  )
+  const data = await response.json()
+  await db.pokemon.bulkPut(data.results)
 })
 
 db.open()
 
-const filteredTasks = await db.tasks
-  .where('userId')
-  .equals(10)
-  .filter((item) => !item.completed)
+const pokemon = await db.pokemon
+  .where('id')
+  .between(1, 20)
+  .filter((poke) => /^b/.test(poke.name))
   .toArray()
 
-console.table(filteredTasks)
+console.table(pokemon)
