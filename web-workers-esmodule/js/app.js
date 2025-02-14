0572f9347e5311a59c50c51db090a0db324a86ca
Edy Segura <edysegura@gmail.com>
2025-02-14 16:54:44 -0300

    feat: enhance web worker functionality and update HTML structure for better user interaction

--- web-workers-esmodule/js/app.js
+++ web-workers-esmodule/js/app.js
@@ -1,8 +1,19 @@
-const url = new URL('worker.js', import.meta.url)
-const worker = new Worker(url, { type: 'module' })
-
-worker.addEventListener('message', ({ data }) => {
-  console.log(data)
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+  doSomethingInAnotherThread()
 })
 
-worker.postMessage('Please do something')
+function doSomethingInAnotherThread() {
+  const url = new URL('worker.js', import.meta.url)
+  const worker = new Worker(url, { type: 'module' })
+  worker.addEventListener('message', ({ data }) => {
+    showOutput(data)
+    worker.terminate() // it is important to terminate the worker when it is no longer needed
+  })
+  worker.postMessage('Please do something')
+}
+
+function showOutput(message) {
+  const outputElement = document.querySelector('pre')
+  outputElement.textContent = message
+}
--- web-workers-esmodule/js/worker.js
+++ web-workers-esmodule/js/worker.js
@@ -1,8 +1,10 @@
 import { times } from 'https://cdn.jsdelivr.net/npm/lodash-es@4.17.21/+esm'
 
 self.onmessage = ({ data }) => {
-  const dataProcessed = []
+  const dataProcessed = [`\t* ${data}`]
   // do something heavy processing here
-  times(3, (index) => dataProcessed.push(`Heavy processing... ${index}`))
-  self.postMessage('You said: ' + dataProcessed.join('\n'))
+  times(3, (index) =>
+    dataProcessed.push(`\t* Heavy processing... ${index + 1}`),
+  )
+  self.postMessage('You said:\n' + dataProcessed.join('\n'))
 }
