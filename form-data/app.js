bff06905e02b3d6c0bd8a6b3139ee741adc6bf83
Edy Segura <edysegura@gmail.com>
2025-03-24 17:29:33 -0300

    feat: add function to display form values after submission in FormData handling

--- form-data/app.js
+++ form-data/app.js
@@ -3,12 +3,15 @@ const pre = document.querySelector('pre')
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
+  showFormValues(form)
+})
+
+function showFormValues(form) {
   const data = new FormData(form)
   const keys = new Set(data.keys())
   const formValues = []
   for (const key of keys) {
-    console.log(data.getAll(key))
     formValues.push({ field: key, values: data.getAll(key) })
   }
   pre.textContent = JSON.stringify(formValues, null, 2)
-})
+}
