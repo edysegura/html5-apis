fd7e7b467753df7f7c0c9fd3eb788259fbc69cbc
Edy Segura <edysegura@gmail.com>
2025-04-17 17:36:48 -0300

    refactor: simplify code by removing unnecessary semicolons and improving consistency

--- local-storage/app.js
+++ local-storage/app.js
@@ -1,42 +1,37 @@
 class App {
-
   constructor() {
-    this.bindButtonListener();
-    this.listLocalStorageValues();
+    this.bindButtonListener()
+    this.listLocalStorageValues()
   }
 
   bindButtonListener() {
-    const form = document.querySelector('form');
+    const form = document.querySelector('form')
     form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      this.saveToStorage(form);
-    });
+      event.preventDefault()
+      this.saveToStorage(form)
+    })
   }
 
   saveToStorage(form) {
-    const key = form.key.value;
-    const value = form.value.value;
+    const key = form.key.value
+    const value = form.value.value
     if (key && value) {
-      localStorage.setItem(key, value);
-      this.listLocalStorageValues();
-      form.reset();
+      localStorage.setItem(key, value)
+      this.listLocalStorageValues()
+      form.reset()
+      form.key.focus()
     }
   }
 
   listLocalStorageValues() {
-    const lsValues = document.getElementById('lsValues');
-
+    const lsValues = document.getElementById('lsValues')
     const toHtml = (key) => {
-      const value = localStorage.getItem(key);
-      return `<p>${key}: ${value}</p>`;
-    };
-
-    const htmlOutput = Object.keys(localStorage)
-      .map(toHtml)
-      .join('');
-
-    lsValues.innerHTML = htmlOutput;
+      const value = localStorage.getItem(key)
+      return `<p>${key}: ${value}</p>`
+    }
+    const htmlOutput = Object.keys(localStorage).map(toHtml).join('')
+    lsValues.innerHTML = htmlOutput
   }
 }
 
-new App();
+new App()
