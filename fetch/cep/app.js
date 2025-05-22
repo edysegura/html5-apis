8af45fc82d49343e2566d61463f05e20e90a403e
Edy Segura <edysegura@gmail.com>
2025-05-22 17:36:02 -0300

    fix: enhance form submission experience with loading state indication

--- fetch/cep/app.js
+++ fetch/cep/app.js
@@ -1,9 +1,11 @@
 const form = document.querySelector('form')
+const submitButton = document.querySelector('[type="submit"]')
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault()
-  showCepData('loading...')
+  submitButton.setAttribute('aria-busy', true)
   showCepData(await fetchCep(form.cep.value))
+  submitButton.setAttribute('aria-busy', false)
 })
 
 function showCepData(cepData) {
@@ -11,6 +13,7 @@ function showCepData(cepData) {
   pre.textContent = JSON.stringify(cepData, null, 2)
 }
 
+// TODO: implement the other endpoints with promise.race
 async function fetchCep(cep) {
   const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
   const data = await response.json()
