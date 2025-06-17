5210799b7c7e73096c11c8ec84a8491c7e8a7127
Edy Segura <edysegura@gmail.com>
2025-06-17 13:22:10 -0300

    feat: enhance image upload experience with preview and metadata display

--- image-metadata/app.js
+++ image-metadata/app.js
@@ -3,11 +3,14 @@ import Leaflet from 'https://cdn.jsdelivr.net/npm/leaflet@1.9.3/+esm'
 
 const inputFile = document.querySelector('input')
 const preview = document.querySelector('img')
+const placeholder = document.querySelector('.placeholder')
 
 function showImagePreview(file) {
   const fileReader = new FileReader()
   fileReader.addEventListener('loadend', (event) => {
     preview.src = event.target.result
+    preview.style.display = 'block'
+    placeholder.style.display = 'none'
     showImageMetadata(preview.src)
   })
   fileReader.readAsDataURL(file)
@@ -15,7 +18,14 @@ function showImagePreview(file) {
 
 inputFile.addEventListener('change', (event) => {
   const [file] = event.target.files
-  showImagePreview(file)
+  if (file) {
+    showImagePreview(file)
+  } else {
+    preview.style.display = 'none'
+    placeholder.style.display = 'block'
+    preview.src = ''
+    document.querySelector('pre').textContent = ''
+  }
 })
 
 async function showImageMetadata(imageUrl) {
@@ -23,12 +33,12 @@ async function showImageMetadata(imageUrl) {
   const output = await exifr.parse(imageUrl)
   const pre = document.querySelector('pre')
   pre.textContent = JSON.stringify(output, null, 2)
-  if(output) {
+  if (output) {
     showMap(output)
   }
 }
 
-function showMap({latitude, longitude}) {
+function showMap({ latitude, longitude }) {
   const map = Leaflet.map('map').setView([latitude, longitude], 13)
 
   Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
