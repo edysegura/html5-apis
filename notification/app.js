5473e52ee339983f8c6f362b2553383b4ecc7e37
Edy Segura <edysegura@gmail.com>
2025-06-23 19:01:15 -0300

    feat: enhance notification demo with improved HTML structure and button icons

--- notification/app.js
+++ notification/app.js
@@ -1,16 +1,17 @@
 'use strict'
 
-const [ enableButton, notificationButton ] = document.querySelectorAll('button')
+const [enableButton, notificationButton] = document.querySelectorAll('button')
 
 function enableNotification() {
-  Notification.requestPermission()
-    .then(permission => console.log('Notification permission: ' + permission))
+  Notification.requestPermission().then((permission) =>
+    console.log('Notification permission: ' + permission),
+  )
 }
 
 function showNotification() {
   const notificationParams = {
     body: 'This is the notification body, enjoy using this feature!',
-    icon: 'https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_48-512.png'
+    icon: 'https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_48-512.png',
   }
   const notification = new Notification('Warning', notificationParams)
   const delay = 5000
