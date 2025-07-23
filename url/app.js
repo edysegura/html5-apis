e46784a94982f606b1b6ff75b21e4cbe9aa7da54
Edy Segura <edysegura@gmail.com>
2025-07-23 17:03:10 -0300

    chore: lines cleanup

--- url/app.js
+++ url/app.js
@@ -4,10 +4,8 @@ const pre = document.querySelector('pre')
 
 function processUrl(event) {
   event.preventDefault()
-
   const url = new URL(form.url.value)
   const queryParams = Object.fromEntries(url.searchParams.entries())
-
   pre.textContent = JSON.stringify(queryParams, null, 2)
 }
 
