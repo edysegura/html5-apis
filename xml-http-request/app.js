19367f834b4ddedec786a4657a8e3f607aac1165
Edy Segura <edysegura@gmail.com>
2025-08-05 21:53:34 -0300

    refactor: enhance XMLHttpRequest handling and response logging

--- xml-http-request/app.js
+++ xml-http-request/app.js
@@ -7,16 +7,34 @@ button.addEventListener('click', () => {
   doXMLHttpRequest()
 })
 
-function showResponse(json) {
-  pre.textContent = json
-}
-
 function doXMLHttpRequest() {
-  pre.textContent = 'loading...'
   const request = new XMLHttpRequest()
   request.open('GET', 'https://api.github.com/users/edysegura')
-  request.addEventListener('load', (event) =>
-    showResponse(event.target.responseText),
-  )
+  request.addEventListener('loadstart', () => {
+    console.log('---> HTTP request started')
+    pre.textContent = 'loading...'
+  })
+  request.addEventListener('readystatechange', (event) => {
+    console.log('---> State change:', event.target.readyState)
+  })
+  request.addEventListener('load', (event) => {
+    console.log('---> HTTP request completed')
+    console.log('---> Status:', event.target.status)
+    logResponseHeaders(event.target)
+    pre.textContent = event.target.responseText
+  })
   request.send()
+}
+
+function logResponseHeaders(request) {
+  console.log('---> Response Headers:')
+  const headersString = request.getAllResponseHeaders()
+  const headers = headersString
+    .trim()
+    .split(/[\r\n]+/)
+    .map((line) => {
+      const [key, value] = line.split(': ')
+      return { key, value }
+    })
+  console.table(headers)
 }
