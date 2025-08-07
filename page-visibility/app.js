c532ace87701970ad47861757363d338e6294adc
Edy Segura <edysegura@gmail.com>
2025-08-07 20:34:56 -0300

    feat: add clock emoji

--- page-visibility/app.js
+++ page-visibility/app.js
@@ -11,7 +11,7 @@ const logCurrentState = (event) => {
   const visibilityState = event.target.visibilityState
   const message = documentStates[visibilityState]
   const timestamp = new Date().toLocaleTimeString()
-  logArea.value += `[${timestamp}] ${message}\n`
+  logArea.value += `[⏱️${timestamp}] ${message}\n`
   logArea.scrollTop = logArea.scrollHeight
 }
 
