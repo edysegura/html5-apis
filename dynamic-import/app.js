051c3aad026406a7c893ceaeaa3da09ac5fcf1a3
Edy Segura <edysegura@gmail.com>
2025-09-17 17:38:23 -0300

    feat: display loaded users in the DOM after fetching from JSON

--- dynamic-import/app.js
+++ dynamic-import/app.js
@@ -3,6 +3,20 @@ const button = document.querySelector('button')
 button.addEventListener('click', loadUsers)
 
 async function loadUsers() {
-  const data = await import('./data.json', { with: { type: 'json' } })
-  console.log('loading users...', data)
+  const module = await import('./data.json', { with: { type: 'json' } })
+  const {
+    default: { users },
+  } = module
+  console.log('loading users...', users)
+  listUsers(users)
+}
+
+function listUsers(users) {
+  const div = document.querySelector('#users')
+  const html = `
+    <uL>
+      ${users.map((user) => `<li>${user}</li>`).join('')}
+    </ul>
+  `
+  div.innerHTML = html
 }
