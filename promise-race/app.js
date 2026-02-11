1f93872d47e629bfe00deecb5137990a5caddda0
Edy Segura <edysegura@gmail.com>
2026-02-10 21:50:14 -0300

    feat: add a third option to fetch the cep

--- promise-race/app.js
+++ promise-race/app.js
@@ -1,10 +1,12 @@
 // implement the promise race to request two free cep rest api and return the first response
 const cep1 = 'https://viacep.com.br/ws/01001000/json/'
 const cep2 = 'https://cep.awesomeapi.com.br/json/01001000'
+const cep3 = 'https://brasilapi.com.br/api/cep/v1/01001000'
 
 Promise.race([
   fetch(cep1).then((response) => response.json()),
   fetch(cep2).then((response) => response.json()),
+  fetch(cep3).then((response) => response.json()),
 ])
   .then((data) => {
     console.log('First response:', data)
