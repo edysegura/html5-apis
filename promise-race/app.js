4d80017e3d8125067a7a757398cd444543f9996a
Edy Segura <edysegura@gmail.com>
2026-02-12 21:50:14 -0300

    refactor: use ES modules

--- promise-race/app.js
+++ promise-race/app.js
@@ -1,45 +1,4 @@
-function normalizeResponse(data) {
-  const origin = data.logradouro
-    ? 'viacep'
-    : data.address_type
-      ? 'cep.awesomeapi'
-      : data.service
-        ? 'brasilapi'
-        : 'unknown'
-
-  return {
-    origin,
-    cep: data.cep || '',
-    street: data.logradouro || data.address || data.street || '',
-    complement: data.complemento || '',
-    district: data.bairro || data.district || data.neighborhood || '',
-    city: data.localidade || data.city || '',
-    state: data.uf || data.state || '',
-    ddd: data.ddd || '',
-    lat: data.lat || '',
-    lng: data.lng || '',
-  }
-}
-
-async function fetchCepData(cep) {
-  const endpoints = [
-    `https://viacep.com.br/ws/${cep}/json/`,
-    `https://cep.awesomeapi.com.br/json/${cep}`,
-    `https://brasilapi.com.br/api/cep/v1/${cep}`,
-  ]
-
-  const cepPromises = endpoints.map((endpoint) =>
-    fetch(endpoint).then((response) => response.json()),
-  )
-
-  try {
-    const data = await Promise.race(cepPromises)
-    return normalizeResponse(data)
-  } catch (error) {
-    console.error('Error:', error)
-    throw error
-  }
-}
+import { fetchCepData } from './cep/cep.service.js'
 
 fetchCepData('01310100')
   .then((data) => {
