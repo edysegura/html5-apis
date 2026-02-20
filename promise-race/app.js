d44bc357c9873d74874fb3064653864db87201c6
Edy Segura <edysegura@gmail.com>
2026-02-19 22:25:30 -0300

    refactor: app.js

--- promise-race/app.js
+++ promise-race/app.js
@@ -1,14 +1,10 @@
 import { fetchCepData } from './cep/cep.service.js'
 
-fetchCepData('01310100')
-  .then((data) => {
-    console.log('Normalized response:', data)
-    document.getElementById('output').textContent = JSON.stringify(
-      data,
-      null,
-      2,
-    )
-  })
-  .catch((error) => {
-    console.error('Failed to fetch CEP data:', error)
-  })
+try {
+  const outputElement = document.getElementById('output')
+  const data = await fetchCepData('01310100')
+  outputElement.textContent = JSON.stringify(data, null, 2)
+} catch (error) {
+  console.error('Failed to fetch CEP data:', error)
+  outputElement.textContent = `Failed to fetch CEP data. Error: ${error.message}`
+}
