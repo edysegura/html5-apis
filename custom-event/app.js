7027d021b3db0e05f78ba5ed47f1759dafc6c5f3
Edy Segura <edysegura@gmail.com>
2026-03-18 21:37:42 -0300

    feat: update event logging to prepend new entries in the event log

--- custom-event/app.js
+++ custom-event/app.js
@@ -1,17 +1,14 @@
 const button = document.querySelector('#trigger')
 const eventLog = document.getElementById('eventLog')
 
-// Create a new event, allow bubbling, and provide any data you want to pass to the "detail" property
 const eventAwesome = new CustomEvent('awesome', {
   bubbles: true,
   detail: { text: () => 'Hello, World from a custom event!' },
 })
 
-// The form element listens for the custom "awesome" event and then consoles the output of the passed text() method
 document.addEventListener('awesome', (event) => {
   console.log(`👁️ [app.js] `, event.detail.text())
-  eventLog.value += `[${new Date().toLocaleString()}] ${event.detail.text()}\n`
+  eventLog.value = `[${new Date().toLocaleString()}] ${event.detail.text()}\n${eventLog.value}`
 })
 
-// As the user types, the textarea inside the form dispatches/triggers the event to fire, and uses itself as the starting point
 button.addEventListener('click', () => document.dispatchEvent(eventAwesome))
