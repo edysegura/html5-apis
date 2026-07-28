20f0dd5a7eb31930dbec281aa8c135ff1a973791
Edy Segura <edysegura@gmail.com>
2026-07-27 21:36:27 -0300

    feat: implement SSE with htmx

--- server-sent-events/server/nodejs/server.mjs
+++ server-sent-events/server/nodejs/server.mjs
@@ -15,6 +15,8 @@ const server = http.createServer((request, response) => {
     'Cache-Control': 'no-cache',
     Connection: 'keep-alive',
     'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
   })
 
   response.flushHeaders()
