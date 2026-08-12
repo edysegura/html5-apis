8030057d1ca5f27155390a0439a9d00739f9ea0c
Edy Segura <edysegura@gmail.com>
2026-08-11 21:47:39 -0300

    feat: update button IDs and event listeners for improved functionality in Abort Controller example

--- abort-controller/app.js
+++ abort-controller/app.js
@@ -1,12 +1,21 @@
-const btnClick = document.getElementById('btn-click')
+const btnClick1 = document.getElementById('btn-click1')
+const btnClick2 = document.getElementById('btn-click2')
 const btnAbort = document.getElementById('btn-abort')
 
 const controller = new AbortController()
 
-btnClick.addEventListener(
+btnClick1.addEventListener(
   'click',
   (event) => {
-    logMessage(`👁️ [app.js] button clicked`)
+    logMessage(`👁️ [app.js] button 1 clicked`)
+  },
+  { signal: controller.signal },
+) // pass an AbortSignal to this handler
+
+btnClick2.addEventListener(
+  'click',
+  (event) => {
+    logMessage(`👁️ [app.js] button 2 clicked`)
   },
   { signal: controller.signal },
 ) // pass an AbortSignal to this handler
